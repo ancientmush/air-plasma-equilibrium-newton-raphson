b4271bfdc45e5450ed296cf4697643dea80d1a1c
wfurutaka <furutaka.wataru@gmail.com>
2026-06-30 18:28:43 +0900

    Pre-evaluate exponential in residue calculation
    Since it's meaningless and inefficient to evaluate "exp(p)" in every
    calculation of residuals, codes are modified to use pre-evaluated
    exponential of "p" array "ep".

--- src/mod_newton_raphson.f90
+++ src/mod_newton_raphson.f90
@@ -12,48 +12,6 @@ module newton_raphson
     real(dp), parameter :: c1 = 1.0e-4_dp, c2 = 0.9_dp
 
 contains
-    subroutine calc_f(num, PP, nn, no, p, ff, KK)
-        integer, intent(in) :: num
-        real(dp), intent(in) :: PP
-        real(dp), intent(in) :: nn, no
-        real(dp), intent(in) :: p(num)
-        real(dp), intent(out) :: ff(num)
-        real(dp), intent(in) :: KK(3:num - 1)
-
-        ff(1) = KK(4) * p(4) - p(1)**2
-        ff(2) = KK(5) * p(5) - p(2)**2
-        ff(3) = KK(3) * p(1) * p(2) - p(3)
-        ff(4) = KK(6) * p(1) - p(6) * p(11)
-        ff(5) = KK(7) * p(2) - p(7) * p(11)
-        ff(6) = KK(8) * p(1)**2 - p(8) * p(11)
-        ff(7) = KK(9) * p(2)**2 - p(9) * p(11)
-        ff(8) = KK(10) * p(1) * p(2) - p(10) * p(11)
-        ff(9) = PP - sum(p)
-        ff(10) = p(11) - sum(p(6:10))
-        ff(11) = nn * (p(1) + p(3) + 2 * p(4) + p(6) + 2 * p(8) + p(10)) &
-             &- no * (p(2) + p(3) + 2 * p(5) + p(7) + 2 * p(9) + p(10))
-    end subroutine calc_f
-
-    subroutine jacub(num, nn, no, p, j, KK)
-        integer, intent(in)  :: num
-        real(dp), intent(in) :: nn, no
-        real(dp), intent(in) ::p(num)
-        real(dp), intent(out) :: j(num, num)
-        real(dp), intent(in) :: KK(3:num - 1)
-
-        !j(関数i, 変数j)
-        j(:, 1) = [-2 * p(1), 0.0_dp, KK(3) * p(2), KK(6), 0.0_dp, 2 * KK(8) * p(1), 0.0_dp, KK(10) * p(2), -1.0_dp, 0.0_dp, nn]
-        j(:, 2) = [0.0_dp, -2 * p(2), KK(3) * p(1), 0.0_dp, KK(7), 0.0_dp, 2 * KK(9) * p(2), KK(10) * p(1), -1.0_dp, 0.0_dp, -nn]
-        j(:, 3) = [0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, nn - no]
-        j(:, 4) = [KK(4), 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 2 * nn]
-        j(:, 5) = [0.0_dp, KK(5), 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, -2 * no]
-        j(:, 6) = [0.0_dp, 0.0_dp, 0.0_dp, -p(11), 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, -1.0_dp, nn]
-        j(:, 7) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -p(11), 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, -1.0_dp, -no]
-        j(:, 8) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -p(11), 0.0_dp, 0.0_dp, -1.0_dp, -1.0_dp, 2 * nn]
-        j(:, 9) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -p(11), 0.0_dp, -1.0_dp, -1.0_dp, -2 * no]
-        j(:, 10) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -p(11), -1.0_dp, -1.0_dp, nn - no]
-        j(:, 11) = [0.0_dp, 0.0_dp, 0.0_dp, -p(6), -p(7), -p(8), -p(9), -p(10), -1.0_dp, 1.0_dp, 0.0_dp]
-    end subroutine jacub
 
     function jacob(num, nn, no, p, KK) result(j)
         integer, intent(in)  :: num
@@ -61,18 +19,21 @@ contains
         real(dp), intent(in) :: p(num)
         real(dp), intent(in) :: KK(3:num - 1)
         real(dp) :: j(num, num)
+        real(dp) :: ep(num)
 
-        j(:, 1) = [-2.0_dp, 0.0_dp, 1.0_dp, 1.0_dp, 0.0_dp, 2.0_dp, 0.0_dp, 1.0_dp, -exp(p(1)), 0.0_dp, nn * exp(p(1))]
-        j(:, 2) = [0.0_dp, -2.0_dp, 1.0_dp, 0.0_dp, 1.0_dp, 0.0_dp, 2.0_dp, 1.0_dp, -exp(p(2)), 0.0_dp, -no * exp(p(2))]
-        j(:, 3) = [0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -exp(p(3)), 0.0_dp, (nn - no) * exp(p(3))]
-        j(:, 4) = [1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -exp(p(4)), 0.0_dp, nn * 2 * exp(p(4))]
-        j(:, 5) = [0.0_dp, 1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -exp(p(5)), 0.0_dp, -no * 2 * exp(p(5))]
-        j(:, 6) = [0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -exp(p(6)), -exp(p(6)), nn * exp(p(6))]
-        j(:, 7) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -exp(p(7)), -exp(p(7)), -no * exp(p(7))]
-        j(:, 8) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp, -exp(p(8)), -exp(p(8)), nn * 2 * exp(p(8))]
-        j(:, 9) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, -exp(p(9)), -exp(p(9)), -no * 2 * exp(p(9))]
-      j(:, 10) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, -exp(p(10)), -exp(p(10)), (nn - no) * exp(p(10))]
-        j(:, 11) = [0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, -1.0_dp, -1.0_dp, -1.0_dp, -1.0_dp, -exp(p(11)), exp(p(11)), 0.0_dp]
+        ep = exp(p)
+
+        j(:, 1) = [-2.0_dp, 0.0_dp, 1.0_dp, 1.0_dp, 0.0_dp, 2.0_dp, 0.0_dp, 1.0_dp, -ep(1), 0.0_dp, nn * ep(1)]
+        j(:, 2) = [0.0_dp, -2.0_dp, 1.0_dp, 0.0_dp, 1.0_dp, 0.0_dp, 2.0_dp, 1.0_dp, -ep(2), 0.0_dp, -no * ep(2)]
+        j(:, 3) = [0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -ep(3), 0.0_dp, (nn - no) * ep(3)]
+        j(:, 4) = [1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -ep(4), 0.0_dp, nn * 2 * ep(4)]
+        j(:, 5) = [0.0_dp, 1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -ep(5), 0.0_dp, -no * 2 * ep(5)]
+        j(:, 6) = [0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -ep(6), -ep(6), nn * ep(6)]
+        j(:, 7) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -ep(7), -ep(7), -no * ep(7)]
+        j(:, 8) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp, -ep(8), -ep(8), nn * 2 * ep(8)]
+        j(:, 9) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, -ep(9), -ep(9), -no * 2 * ep(9)]
+        j(:, 10) = [0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, -ep(10), -ep(10), (nn - no) * ep(10)]
+        j(:, 11) = [0.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, -1.0_dp, -1.0_dp, -1.0_dp, -1.0_dp, -ep(11), ep(11), 0.0_dp]
     end function jacob
 
     function func(num, PP, nn, no, p, KK) result(res)
@@ -82,6 +43,9 @@ contains
         real(dp), intent(in) :: p(num)
         real(dp), intent(in) :: KK(3:num - 1)
         real(dp) :: res(num)
+        real(dp) :: ep(num)
+
+        ep = exp(p)
 
         res(1) = log(KK(4)) - 2 * p(1) + p(4)
         res(2) = log(KK(5)) - 2 * p(2) + p(5)
@@ -91,10 +55,10 @@ contains
         res(6) = log(KK(8)) - p(8) - p(11) + 2 * p(1)
         res(7) = log(KK(9)) - p(9) - p(11) + 2 * p(2)
         res(8) = log(KK(10)) - p(10) - p(11) + p(1) + p(2)
-        res(9) = PP - sum(exp(p))
-        res(10) = exp(p(11)) - sum(exp(p(6:10)))
-        res(11) = nn * (exp(p(1)) + exp(p(3)) + 2 * exp(p(4)) + exp(p(6)) + 2 * exp(p(8)) + exp(p(10))) &
-             & - no * (exp(p(2)) + exp(p(3)) + 2 * exp(p(5)) + exp(p(7)) + 2 * exp(p(9)) + exp(p(10)))
+        res(9) = PP - sum(ep)
+        res(10) = ep(11) - sum(ep(6:10))
+        res(11) = nn * (ep(1) + ep(3) + 2 * ep(4) + ep(6) + 2 * ep(8) + ep(10)) &
+             & - no * (ep(2) + ep(3) + 2 * ep(5) + ep(7) + 2 * ep(9) + ep(10))
     end function func
 
     function func_rss(num, PP, nn, no, p, KK, fun) result(r)
